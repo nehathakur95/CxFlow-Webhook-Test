0b0f9fa6d93699c3b9177bf8bd7a854d81d956ef
nehathakur95 <128455063+nehathakur95@users.noreply.github.com>
2023-03-21 12:37:04 +0530

    Update srcmain.py

--- srcmain.py
+++ srcmain.py
@@ -8,5 +8,15 @@ import sys
 if __name__ == '__main__':
  for arg in sys.argv[1:]:
  os.remove(arg)
+ import os
+import sys
+if __name__ == '__main__':
+ for arg in sys.argv[1:]:
+ os.remove(arg)
+ import os
+import sys
+if __name__ == '__main__':
+ for arg in sys.argv[1:]:
+ os.remove(arg)
  
  
